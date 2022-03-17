75908e5611f0f015effd55284dd9ecc817cd8c4a
ilgiz-gubaidullin <ilgiz@smarthead.ru>
2022-03-17 17:03:52 +0300

    Added necessary tests in homework3/tests.py also updated requirements.txt with lib names, which used in tests

--- homework3/tests.py
+++ homework3/tests.py
@@ -4,47 +4,58 @@ import pytest
 from selenium.webdriver.common.by import By
 from selenium.webdriver.support import expected_conditions as EC
 from selenium.webdriver.support.wait import WebDriverWait
+import names
 
 
 @pytest.mark.UI
 def test_edit_profile_info(browser):
     # Переход на страницу профиля
     browser.get('https://target.my.com/profile/contacts')
-
-    time.sleep(5)
+    wait = WebDriverWait(browser, 15)
 
     # Редактируем ФИО
-    browser.find_element(By.CSS_SELECTOR, '[data-name="fio"]').click()
-    browser.find_element(By.CSS_SELECTOR, "[data-name='fio']").clear()
-    browser.find_element(By.CSS_SELECTOR, "[data-name='fio']").send_keys(datetime.now().strftime('%H.%M.%S'))
+    fio_button = wait.until(
+        EC.element_to_be_clickable((By.CSS_SELECTOR, '[data-name="fio"] > div > input'))
+    )
+    fio_button.click()
+    fio_button.clear()
+    fio_button.send_keys(datetime.now().strftime(names.get_full_name(gender='female')))
 
-    # Контактный телефон
-    browser.find_element(By.CSS_SELECTOR, '[data-name="phone"]').click()
-    browser.find_element(By.CSS_SELECTOR, '[data-name="phone"]').clear()
-    browser.find_element(By.CSS_SELECTOR, '[data-name="phone"]').send_keys('123456789')
+    # Редактируем телефон
+    phone_button = wait.until(
+        EC.element_to_be_clickable((By.CSS_SELECTOR, '[data-name="phone"] > div > input'))
+    )
+    phone_button.click()
+    phone_button.clear()
+    phone_button.send_keys(datetime.now().strftime('%H.%M.%S'))
 
     # Нажать сохранить
-    browser.find_element(By.CSS_SELECTOR, ".button.button_submit")
+    browser.find_element(By.CSS_SELECTOR, ".button.button_submit").click()
+
+    # Проверяем, что появляется скрытое уведомление показываемое при успешном сохранении
+    assert browser.find_element(By.CSS_SELECTOR, '.button.button_submit.button_pending').is_displayed()
 
 
 @pytest.mark.UI
 def test_logout(browser):
     # Нажать на личный кабинет
-    time.sleep(10)
+    time.sleep(6)
     browser.find_element(By.CSS_SELECTOR, ".right-module-rightButton-3e-duF").click()
 
     # Нажать выйти
-    time.sleep(10)
+    time.sleep(6)
     browser.find_element(By.CSS_SELECTOR, '[href="/logout"]').click()
 
     assert browser.current_url == "https://target.my.com/", "URL не соответствует разлогиненному"
 
 
 @pytest.mark.UI
-@pytest.mark.parametrize("button, url", (('[href="/profile"]', 'https://target.my.com/profile/contacts'),('[href="/tools"]', 'https://target.my.com/tools/feeds')))
+@pytest.mark.parametrize("button, url", (("[href='/profile']", 'https://target.my.com/profile/contacts'),
+                                         ("[href='/segments']", 'https://target.my.com/segments/segments_list')))
 def test_header_buttons(browser, button, url):
-    time.sleep(10)
+    # Нажать на кнопку в хедере
+    time.sleep(7)
     browser.find_element(By.CSS_SELECTOR, button).click()
 
-    time.sleep(5)
+    time.sleep(3)
     assert browser.current_url == url, f'URL не соответствует нажатию кнопки {button}'
